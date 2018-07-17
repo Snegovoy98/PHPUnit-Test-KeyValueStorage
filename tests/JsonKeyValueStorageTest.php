f24866001bea882ac364ec251c26925129d68cc7
Snegovoy98 <snegovoy.aleksandr@gmail.com>
2018-07-17 21:56:28 +0300

    Add methods setUp and tearDown, delete method testClear, change pathToFIle on const

--- tests/JsonKeyValueStorageTest.php
+++ tests/JsonKeyValueStorageTest.php
@@ -7,49 +7,61 @@ use PHPUnit\Framework\TestCase;
 
 class JsonKeyValueStorageTest extends TestCase
 {
-    private $storage;
+    protected const Path_To_File = __DIR__.'/../data/storage.json';
 
-    public function __construct()
+    /**
+     * @var JsonKeyValueStorage
+     */
+    protected  $storage ;
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function setUp()
     {
-        $this->storage = new JsonKeyValueStorage(__DIR__ . '../data/storage.json');
+        $this->storage = new JsonKeyValueStorage(self::Path_To_File);
     }
 
+    /**
+     * {@inheritdoc}
+     */
+    public function tearDown()
+    {
+        \file_put_contents(self::Path_To_File, \LOCK_EX);
+    }
+
+    /**
+     * @expectedException \Snegovoy\App\Test
+     * @expectedExceptionMessage You should specify path to file
+     */
     public function testSet()
     {
-        $this->storage->clear();
         $this->storage->set('array', ['domain.com', 'web_developer.com.ua', 'php_develop.com']);
-        $this->assertEquals(['domain.com'], $this->storage->get('array'));
-
+        self::assertEquals(['domain.com', 'web_developer.com.ua', 'php_develop.com'], $this->storage->get('array'));
     }
 
     public function testGet()
     {
-        $this->clearFile();
         $this->storage->set('name', 'Igor');
         $this->storage->set('date', 'June 28');
-        $this->assertEquals('Igar', $this->storage->get('name'));
-        $this->assertEquals('August', $this->storage);
-
-
+        self::assertEquals('Igor', $this->storage->get('name'));
+        self::assertEquals('August', $this->storage->get('data'));
     }
 
     public function testHas()
     {
-        $this->clearFile();
         $this->storage->set('car', 'Audi');
         $this->storage->set('mode', 'R8');
-        $this->assertEquals(true, $this->storage->has('car'));
-        $this->assertEquals(false, $this->storage->has('model'));
+        self::assertEquals(true, $this->storage->has('car'));
+        self::assertEquals(false, $this->storage->has('model'));
 
     }
 
     public function testRemove()
     {
-        $this->clearFile();
         $this->storage->set('date', 'June');
         $this->storage->remove('date');
-        $this->assertEquals(true, $this->storage->get('date'));
-
+        self::assertEquals(true, $this->storage->get('date'));
     }
 
     public function testClear()
@@ -57,11 +69,6 @@ class JsonKeyValueStorageTest extends TestCase
         $this->storage->set('products', ['Iphone', 'Mac', 'AppWatch']);
         $this->storage->set('producer', 'Apple');
         $this->storage->clear();
-        $this->assertEquals(true, $this->storage->has('producer'));
-    }
-
-    private function clearFile()
-    {
-        \file_put_contents($this->storage,'');
+        self::assertEquals(true, $this->storage->has('producer'));
     }
 }
